4c91ffc20e334114eca4edd23864869b63e724d8
Jonathan White <jawh@umich.edu>
2024-01-29 20:41:10 -0700

    Defined DerivativeFunc concept for constraining derivative parameter.

--- src/ode.hpp
+++ src/ode.hpp
@@ -1,8 +1,14 @@
 #include <concepts>
 #include <ranges>
 
-template <std::ranges::random_access_range StateType, std::floating_point StepType, typename DerivFunc>
-auto euler_step(StateType& state, StepType dt, DerivFunc f)
+template <typename Callable, typename StateType>
+concept DerivativeFunc = std::invocable<Callable> &&
+    std::ranges::random_access_range<std::invoke_result_t<Callable>> &&
+    std::ranges::random_access_range<StateType> &&
+    std::convertible_to<std::invoke_result_t<Callable>, StateType>;
+
+template <std::ranges::random_access_range StateType, std::floating_point StepType, DerivativeFunc<StateType> F>
+auto euler_step(StateType& state, StepType dt, F f)
 {
     auto const& derivative = f();
     for (auto&& [x, dx_dt] : std::views::zip(state, derivative))

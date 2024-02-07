a3de3b088eea1485909f3bf3e85a3b8434692ab4
Jonathan White <jawh@umich.edu>
2024-02-07 12:33:37 -0700

    Added simple oscillator test to Numerical Recipes test.

--- src/test/nr_17_0_4_test.cpp
+++ src/test/nr_17_0_4_test.cpp
@@ -37,3 +37,36 @@ TEST(ODEIntegratorTest, VanDerPolTest)
     EXPECT_DOUBLE_EQ( 1.7644320190605265,  ystart[0]);
     EXPECT_DOUBLE_EQ(-0.83427005245677999, ystart[1]);
 }
+
+struct RHSOsc
+{
+    void operator()(const double x, std::vector<double>& y, std::vector<double>& dydx)
+    {
+        dydx[0] = y[1];
+        dydx[1] = -y[0];
+    }
+};
+
+TEST(ODEIntegratorTest, SimpleOscillatorTest)
+{
+    const int nvar = 2;
+    const double atol = 1.0e-10;
+    const double rtol = atol;
+    const double h1 = 0.01;
+    const double hmin = 0.0;
+    const double x1 = 0.0;
+    const double x2 = 2.0;
+    std::vector<double> ystart(nvar);
+    ystart[0] = 1.0;
+    ystart[1] = 0.0;
+    Output out(20);
+    RHSOsc d;
+    ODEIntegrator<StepperDopr5<RHSOsc>> ode(ystart, x1, x2, atol, rtol, h1,
+        hmin, out, d);
+
+    ode.integrate();
+
+    // Values take from initial run. Testing for consistency.
+    EXPECT_NEAR(-0.4161468365, ystart[0], atol);
+    EXPECT_NEAR(-0.9092974268, ystart[1], atol);
+}

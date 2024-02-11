81d7befdf9106f8ba7fa2206f636152880656781
Jonathan White <jawh@umich.edu>
2024-02-11 15:46:29 -0700

    Replaced manual management of Output vector allocations with std::vector interface.

--- src/Output.hpp
+++ src/Output.hpp
@@ -5,7 +5,6 @@
 
 /* Structure for output from ODE solver such as ODEIntegrator. */
 class Output {
-  int kmax;  // Current capacity of storage arrays.
   int nvar;
   int nsave;   // Number of intervals to save at for dense output.
   bool dense;  // true if dense output requested.
@@ -22,17 +21,14 @@ class Output {
  public:
   static constexpr auto init_cap = 500;  // Initial capacity of storage arrays.
   /* Default constructor gives no output. */
-  Output() : kmax(-1), suppress_output{true}, dense(false), count(0) {}
+  Output() : suppress_output{true}, dense(false), count(0) {}
 
   /* Constructor provides dense output at nsave equally spaced intervals. If
    * nsave <= 0, output is saved only at the actual integration steps. */
   Output(int nsavee)
-      : kmax(init_cap),
-        suppress_output{false},
-        nsave(nsavee),
-        count(0),
-        xsave(init_cap) {
+      : suppress_output{false}, nsave(nsavee), count(0), xsave{} {
     dense = nsave > 0;
+    xsave.reserve(init_cap);
   }
 
   /* Called by the ODEIntegrator constructor, which passes neqn, the number of
@@ -45,7 +41,7 @@ class Output {
     }
     ysave.resize(nvar);
     for (auto& y : ysave) {
-      y.resize(init_cap);
+      y.reserve(init_cap);
     }
     if (dense) {
       x1 = xlo;
@@ -55,52 +51,29 @@ class Output {
     }
   }
 
-  /* Resize storage arrays by a factor of two, keeping saved data. */
-  void resize() {
-    int kold = kmax;
-    kmax *= 2;
-    std::vector<double> tempvec(xsave);
-    xsave.resize(kmax);
-    for (int k = 0; k < kold; ++k) {
-      xsave[k] = tempvec[k];
-    }
-    std::vector<std::vector<double>> tempmat(ysave);
-    ysave.resize(nvar);
-    for (int i = 0; i < nvar; ++i) {
-      ysave[i].resize(kmax);
-      for (int k = 0; k < kold; ++k) {
-        ysave[i][k] = tempmat[i][k];
-      }
-    }
-  }
-
   /* Invokes dense_out function of stepper routine to produce output at xout.
    * Normally called by out rather than directly. Assumes that xout is between
    * xold and xold + h, where the stepper must keep track of xold, the location
    * of the previous step, and x = xold + h, the current step. */
   template <typename Stepper>
   void save_dense(Stepper const& stepper, double xout, double h) {
-    if (count == kmax) {
-      resize();
-    }
     for (int i = 0; i < nvar; ++i) {
-      ysave[i][count] = stepper.dense_out(i, xout, h);
+      ysave[i].push_back(stepper.dense_out(i, xout, h));
     }
-    xsave[count++] = xout;
+    xsave.push_back(xout);
+    count++;
   }
 
   /* Saves values of current x and y. */
   void save(double x, std::vector<double> const& y) {
-    if (kmax <= 0) {
+    if (suppress_output) {
       return;
     }
-    if (count == kmax) {
-      resize();
-    }
     for (int i = 0; i < nvar; ++i) {
-      ysave[i][count] = y[i];
+      ysave[i].push_back(y[i]);
     }
-    xsave[count++] = x;
+    xsave.push_back(x);
+    count++;
   }
 
   /* Typically called by ODEIntegrator to produce dense output. Input variables

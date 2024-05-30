e9a09b8e69f32c3673f81e249847dc489b0eded0
Jonathan White <jawh@umich.edu>
2024-05-30 17:58:04 -0600

    Added consistency tests for the RKEmbedded module.

---
+++ src/RKEmbedded/test/DVERKTest.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+
+#include <array>
+
+#include "DVERK.hpp"
+#include "VectorState.hpp"
+
+class DVERKVanDerPolTest : public testing::Test {
+ protected:
+  auto static constexpr eps = 1.0;
+  auto static constexpr ode_van = [](VectorState<2> const& x,
+                                     VectorState<2>& dxdt) {
+    dxdt[0] = x[1];
+    dxdt[1] = eps * (1.0 - x[0] * x[0]) * x[1] - x[0];
+  };
+  auto static constexpr x0 = std::array{2.0, 0.0};
+  auto static constexpr t0 = 0.0;
+  auto static constexpr tf = 2.0;
+  auto static constexpr tol = 1.0e-10;
+  auto static constexpr atol = std::array{tol, tol};
+  auto static constexpr rtol = atol;
+
+  DVERK<decltype(ode_van), VectorState<2>> integrator{ode_van};
+};
+
+/* Consistency tests (testing for double equality) are to ensure no accidental
+ * algorithm changes are made during refactoring. These tests are far stricter
+ * than the actual requirements. If an intentional change in algorithm results
+ * in small differences in output, these values may be updated. */
+TEST_F(DVERKVanDerPolTest, IntegrationStepsAreConsistent) {
+  integrator.integrate(x0, t0, tf, atol, rtol);
+
+  EXPECT_EQ(42, integrator.times.size());
+  EXPECT_DOUBLE_EQ(0.0, integrator.times.front());
+  EXPECT_DOUBLE_EQ(0.66331345368999683,
+                   integrator.times[integrator.times.size() / 2]);
+  EXPECT_DOUBLE_EQ(2.0, integrator.times.back());
+
+  EXPECT_EQ(42, integrator.states.size());
+  EXPECT_DOUBLE_EQ(2.0, integrator.states.front()[0]);
+  EXPECT_DOUBLE_EQ(0.0, integrator.states.front()[1]);
+  EXPECT_DOUBLE_EQ(1.7433476890385384,
+                   integrator.states[integrator.states.size() / 2][0]);
+  EXPECT_DOUBLE_EQ(-0.61887401617955773,
+                   integrator.states[integrator.states.size() / 2][1]);
+  EXPECT_DOUBLE_EQ(0.32331666651502594, integrator.states.back()[0]);
+  EXPECT_DOUBLE_EQ(-1.8329745684699728, integrator.states.back()[1]);
+}
---
+++ src/RKEmbedded/test/RKF78Test.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+
+#include <array>
+
+#include "RKF78.hpp"
+#include "VectorState.hpp"
+
+class RKF78VanDerPolTest : public testing::Test {
+ protected:
+  auto static constexpr eps = 1.0;
+  auto static constexpr ode_van = [](VectorState<2> const& x,
+                                     VectorState<2>& dxdt) {
+    dxdt[0] = x[1];
+    dxdt[1] = eps * (1.0 - x[0] * x[0]) * x[1] - x[0];
+  };
+  auto static constexpr x0 = std::array{2.0, 0.0};
+  auto static constexpr t0 = 0.0;
+  auto static constexpr tf = 2.0;
+  auto static constexpr tol = 1.0e-10;
+  auto static constexpr atol = std::array{tol, tol};
+  auto static constexpr rtol = atol;
+
+  RKF78<decltype(ode_van), VectorState<2>> integrator{ode_van};
+};
+
+/* Consistency tests (testing for double equality) are to ensure no accidental
+ * algorithm changes are made during refactoring. These tests are far stricter
+ * than the actual requirements. If an intentional change in algorithm results
+ * in small differences in output, these values may be updated. */
+TEST_F(RKF78VanDerPolTest, IntegrationStepsAreConsistent) {
+  integrator.integrate(x0, t0, tf, atol, rtol);
+
+  EXPECT_EQ(15, integrator.times.size());
+  EXPECT_DOUBLE_EQ(0.0, integrator.times.front());
+  EXPECT_DOUBLE_EQ(0.72814921744061389,
+                   integrator.times[integrator.times.size() / 2]);
+  EXPECT_DOUBLE_EQ(2.0, integrator.times.back());
+
+  EXPECT_EQ(15, integrator.states.size());
+  EXPECT_DOUBLE_EQ(2.0, integrator.states.front()[0]);
+  EXPECT_DOUBLE_EQ(0.0, integrator.states.front()[1]);
+  EXPECT_DOUBLE_EQ(1.7022210141241134,
+                   integrator.states[integrator.states.size() / 2][0]);
+  EXPECT_DOUBLE_EQ(-0.64964031512382325,
+                   integrator.states[integrator.states.size() / 2][1]);
+  EXPECT_DOUBLE_EQ(0.3233166666554449, integrator.states.back()[0]);
+  EXPECT_DOUBLE_EQ(-1.8329745686289092, integrator.states.back()[1]);
+}

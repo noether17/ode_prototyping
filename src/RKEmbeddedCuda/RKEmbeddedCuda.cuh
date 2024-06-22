73ad50ab41cd105ac7fa9d2810f264ae20b2fd36
Jonathan White <jawh@umich.edu>
2024-06-22 12:15:12 -0600

    Restarted CUDA implementation of RKEmbedded.

--- src/RKEmbeddedCuda/RKEmbeddedCuda.cuh
+++ src/RKEmbeddedCuda/RKEmbeddedCuda.cuh
@@ -12,136 +12,49 @@ auto consteval num_blocks() {
   return (N + block_size - 1) / block_size;
 }
 
-struct HE21 {
-  auto static constexpr a = std::array<std::array<double, 1>, 1>{{1.0}};
-  auto static constexpr b = std::array{1.0 / 2.0, 1.0 / 2.0};
-  auto static constexpr bt = std::array{1.0, 0.0};
-  auto static constexpr p = 2;
-  auto static constexpr pt = 1;
-  auto static constexpr n_stages = static_cast<int>(b.size());
-};
+// struct HE21 {
+//   auto static constexpr a = std::array<std::array<double, 1>, 1>{{1.0}};
+//   auto static constexpr b = std::array{1.0 / 2.0, 1.0 / 2.0};
+//   auto static constexpr bt = std::array{1.0, 0.0};
+//   auto static constexpr p = 2;
+//   auto static constexpr pt = 1;
+//   auto static constexpr n_stages = static_cast<int>(b.size());
+// };
 
-template <int n_var>
-void rk_norm(double const* v, double const* scale, double* temp,
-             double* result) {
-  elementwise_binary_op_kernel<<<num_blocks<n_var>(), block_size>>>(
-      v, scale, temp, n_var,
-      [] __device__(auto const& v, auto const& scale) { return v / scale; });
-  inner_product_kernel<<<num_blocks<n_var>(), block_size>>>(temp, temp, result,
-                                                            n_var);
-  elementwise_unary_op_kernel<<<1, 1>>>(
-      result, 1, [] __device__(auto& x) { x = std::sqrt(x / n_var); });
+__global__ void cuda_compute_error_target(double const* x, double const* rtol,
+                                          double const* atol,
+                                          double* error_target, int n_var) {
+  auto i = blockIdx.x * blockDim.x + threadIdx.x;
+  while (i < n_var) {
+    error_target[i] = atol[i] + rtol[i] * std::abs(x[i]);
+    i += blockDim.x * gridDim.x;
+  }
 }
 
-template <int n_var>
-void compute_error_target(double const* x, double const* rtol,
-                          double const* atol, double* error_target) {
-  elementwise_binary_op_kernel<<<num_blocks<n_var>(), block_size>>>(
-      x, rtol, error_target, n_var,
-      [] __device__(auto const& x, auto const& rtol) {
-        return rtol * std::abs(x);
-      });
-  elementwise_binary_op_kernel<<<num_blocks<n_var>(), block_size>>>(
-      atol, error_target, n_var,
-      [] __device__(auto const& atol, auto& error_target) {
-        error_target += atol;
-      });
+template <int n_var, typename ODE>
+void cuda_estimate_initial_step(double* dev_x0, double* dev_atol,
+                                double* dev_rtol, double* dev_dt) {
+  double* dev_error_target = nullptr;
+  cudaMalloc(&dev_error_target, n_var * sizeof(double));
+  cuda_compute_error_target<<<num_blocks<n_var>(), block_size>>>(
+      dev_x0, dev_rtol, dev_atol, dev_error_target, n_var);
+
+  // TODO
+
+  cudaFree(dev_error_target);
 }
 
-//__global__ void compute_error_target(double* error_target, double* atol,
-//                                     double* rtol, double* x0) {
-//  auto i = blockIdx.x * blockDim.x + threadIdx.x;
-//  while (i < n_var) {
-//    error_target[i] = atol[i] + rtol[i] * std::abs(x0[i]);
-//    i += blockDim.x * gridDim.x;
-//  }
-//}
-//
-// template <int n_var>
-//__global__ void rk_norm_kernel(double* v, double* scale, double* result) {
-//  __shared__ double cache[block_size];
-//  auto i = blockIdx.x * blockDim.x + threadIdx.x;
-//  auto cache_index = threadIdx.x;
-//  auto temp = 0.0;
-//  while (i < n_var) {
-//    auto scaled_component = v[i] / scale[i];
-//    temp += scaled_component * scaled_component;
-//    i += blockDim.x * gridDim.x;
-//  }
-//  cache[cache_index] = temp;
-//
-//  __syncthreads();
-//  auto reduction_size = blockDim.x;
-//  while (reduction_size != 0) {
-//    if (cache_index < reduction_size) {
-//      cache[cache_index] += cache[cache_index + reduction_size];
-//    }
-//    __syncthreads();
-//    reduction_size /= 2;
-//  }
-//  if (cache_index == 0) {
-//    atomicAdd(result, std::sqrt(cache[0]));
-//  }
-//}
-//
-// template <int n_var>
-// auto rk_norm(double* v, double* scale) {
-//  auto result_dev = static_cast<double*>(nullptr);
-//  cudaMalloc(&result_dev, sizeof(double));
-//  cudaMemset(result_dev, 0, sizeof(double));
-//  rk_norm_kernel<<<num_blocks<n_var>(), block_size>>>(v, scale, result_dev);
-//  auto result = 0.0;
-//  cudaMemcpy(&result, result_dev, sizeof(double), cudaMemcpyDeviceToHost);
-//  cudaFree(result_dev);
-//  return result;
-//}
-//
-// template <int n_var, typename RKMethod, typename ODE>
-// double estimate_initial_step(double* x0_dev, double* atol_dev, double*
-// rtol_dev,
-//                             ODE ode) {
-//  auto error_target_dev = static_cast<double*>(nullptr);
-//  cudaMalloc(&error_target_dev, n_var * sizeof(double));
-//  compute_error_target<<<num_blocks<n_var>(), block_size>>>(
-//      error_target_dev, atol_dev, rtol_dev, x0_dev);
-//
-//  auto f0_dev = static_cast<double*>(nullptr);
-//  cudaMalloc(&f0_dev, n_var * sizeof(double));
-//  ode<<<num_blocks<n_var>(), block_size>>>(x0_dev, f0_dev);
-//  auto d0 = rk_norm(x0, error_target);
-//  auto d1 = rk_norm(f0, error_target);
-//  auto dt0 = (d0 < 1.0e-5 or d1 < 1.0e-5) ? 1.0e-6 : 0.01 * (d0 / d1);
-//
-//  auto x1 = State<n_var>{};
-//  for (int i = 0; i < n_var; ++i) {
-//    x1[i] = x0[i] + dt0 * f0[i];
-//  }
-//  auto f1 = State<n_var>{};
-//  ode(x1, f1);
-//  auto df = State<n_var>{};
-//  for (int i = 0; i < n_var; ++i) {
-//    df[i] = f1[i] - f0[i];
-//  }
-//  auto d2 = rk_norm(df, error_target) / dt0;
-//
-//  auto dt1 =
-//      (std::max(d1, d2) <= 1.0e-15)
-//          ? std::max(1.0e-6, dt0 * 1.0e-3)
-//          : std::pow(0.01 / std::max(d1, d2), (1.0 / (1.0 + RKMethod::p)));
-//  return std::min(100.0 * dt0, dt1);
-//}
-//
-// template <int n_var, typename RKMethod, typename ODE>
-// void RKEmbeddedIntegrate(double* x0, double t0, double tf, double* atol,
-//                         double* rtol, ODE ode) {
-//  auto constexpr a = RKMethod::a;
-//  auto constexpr b = RKMethod::b;
-//  auto constexpr bt = RKMethod::bt;
-//  auto constexpr p = RKMethod::p;
-//  auto constexpr pt = RKMethod::pt;
-//  auto constexpr n_stages = RKMethod::n_stages;
-//
-//  auto ks = std::array<std::array<double, n_var>, n_stages>{};
-//
-//  auto dt = estimate_initial_step(x0, atol, rtol, ode);
-//}
+template <int n_var, typename RKMethod, typename ODE>
+void cuda_integrate(double* dev_x0, double* dev_t0, double* dev_tf,
+                    double* dev_atol, double* dev_rtol) {
+  double* dev_ks = nullptr;
+  cudaMalloc(&dev_ks, n_var * RKMethod::n_stages * sizeof(double));
+  double* dev_dt = nullptr;
+  cudaMalloc(&dev_dt, sizeof(double));
+  cuda_estimate_initial_step<n_var, ODE>(dev_x0, dev_atol, dev_rtol, dev_dt);
+
+  // TODO
+
+  cudaFree(dev_dt);
+  cudaFree(dev_ks);
+}
--- src/RKEmbeddedCuda/test/RKEmbeddedCudaTest.cu
+++ src/RKEmbeddedCuda/test/RKEmbeddedCudaTest.cu
@@ -5,71 +5,7 @@
 #include <memory>
 #include <numeric>
 
-#include "CudaState.cuh"
 #include "RKEmbeddedCuda.cuh"
-
-template <int n_var>
-auto host_rk_norm(std::array<double, n_var> const& v,
-                  std::array<double, n_var> const& scale) {
-  auto scaled_mag = std::inner_product(v.begin(), v.end(), scale.begin(), 0.0,
-                                       std::plus<>{}, [](auto v, auto scale) {
-                                         auto scaled_v = v / scale;
-                                         return scaled_v * scaled_v;
-                                       });
-  return std::sqrt(scaled_mag / n_var);
-}
-
-TEST(RKEmbeddedCudaTest, RKNormTestSmall) {
-  auto constexpr n_var = 10;
-  auto host_v = std::array<double, n_var>{};
-  std::iota(host_v.begin(), host_v.end(), 0.0);
-  auto host_scale = std::array<double, n_var>{};
-  std::iota(host_scale.begin(), host_scale.end(), 1.0);
-  auto dev_v = CudaState<n_var>{host_v};
-  auto dev_scale = CudaState<n_var>{host_scale};
-
-  auto dev_result = CudaState<1>{};
-  auto dev_temp = CudaState<n_var>{};
-  rk_norm<n_var>(dev_v, dev_scale, dev_temp, dev_result);
-
-  auto host_result = host_rk_norm<n_var>(host_v, host_scale);
-  auto host_cuda_result = 0.0;
-  cudaMemcpy(&host_cuda_result, dev_result, sizeof(double),
-             cudaMemcpyDeviceToHost);
-  EXPECT_DOUBLE_EQ(host_result, host_cuda_result);
-}
-
-TEST(RKEmbeddedCudaTest, RKNormTestLarge) {
-  auto constexpr n_var = 1 << 20;
-  auto const tolerance =
-      std::numeric_limits<double>::epsilon() * std::log2(n_var);
-  auto host_v = std::make_unique<std::array<double, n_var>>();
-  std::iota(host_v->begin(), host_v->end(), 0.0);
-  auto host_scale = std::make_unique<std::array<double, n_var>>();
-  std::iota(host_scale->begin(), host_scale->end(), 1.0);
-  auto dev_v = CudaState<n_var>{*host_v};
-  auto dev_scale = CudaState<n_var>{*host_scale};
-
-  auto dev_result = CudaState<1>{};
-  auto dev_temp = CudaState<n_var>{};
-  rk_norm<n_var>(dev_v, dev_scale, dev_temp, dev_result);
-
-  auto host_result = host_rk_norm<n_var>(*host_v, *host_scale);
-  auto host_cuda_result = 0.0;
-  cudaMemcpy(&host_cuda_result, dev_result, sizeof(double),
-             cudaMemcpyDeviceToHost);
-  EXPECT_NEAR(host_result, host_cuda_result, tolerance);
-}
-
-template <int n_var>
-void host_compute_error_target(std::array<double, n_var> const& x,
-                               std::array<double, n_var> const& rtol,
-                               std::array<double, n_var> const& atol,
-                               std::array<double, n_var>& error_target) {
-  for (auto i = 0; i < n_var; ++i) {
-    error_target[i] = atol[i] + rtol[i] * std::abs(x[i]);
-  }
-}
 
 TEST(RKEmbeddedCudaTest, ComputeErrorTargetTestSmall) {
   auto constexpr n_var = 10;
@@ -79,46 +15,74 @@ TEST(RKEmbeddedCudaTest, ComputeErrorTargetTestSmall) {
   std::iota(host_rtol.begin(), host_rtol.end(), 1.0);
   auto host_atol = std::array<double, n_var>{};
   std::iota(host_atol.begin(), host_atol.end(), 2.0);
-  auto dev_x = CudaState<n_var>{host_x};
-  auto dev_rtol = CudaState<n_var>{host_rtol};
-  auto dev_atol = CudaState<n_var>{host_atol};
+  double* dev_x = nullptr;
+  cudaMalloc(&dev_x, n_var * sizeof(double));
+  cudaMemcpy(dev_x, host_x.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_rtol = nullptr;
+  cudaMalloc(&dev_rtol, n_var * sizeof(double));
+  cudaMemcpy(dev_rtol, host_rtol.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_atol = nullptr;
+  cudaMalloc(&dev_atol, n_var * sizeof(double));
+  cudaMemcpy(dev_atol, host_atol.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_error_target = nullptr;
+  cudaMalloc(&dev_error_target, n_var * sizeof(double));
 
-  auto dev_result = CudaState<n_var>{};
-  compute_error_target<n_var>(dev_x, dev_rtol, dev_atol, dev_result);
+  cuda_compute_error_target<<<num_blocks<n_var>(), block_size>>>(
+      dev_x, dev_rtol, dev_atol, dev_error_target, n_var);
 
-  auto host_result = std::array<double, n_var>();
-  host_compute_error_target<n_var>(host_x, host_rtol, host_atol, host_result);
-  auto host_cuda_result = std::array<double, n_var>{};
-  dev_result.to_host(host_cuda_result);
+  auto host_error_target = std::array<double, n_var>{};
+  cudaMemcpy(host_error_target.data(), dev_error_target, n_var * sizeof(double),
+             cudaMemcpyDeviceToHost);
   for (auto i = 0; i < n_var; ++i) {
-    EXPECT_DOUBLE_EQ(host_result[i], host_cuda_result[i]);
+    auto host_result = host_atol[i] + host_rtol[i] * std::abs(host_x[i]);
+    EXPECT_DOUBLE_EQ(host_result, host_error_target[i]);
   }
+
+  cudaFree(dev_error_target);
+  cudaFree(dev_atol);
+  cudaFree(dev_rtol);
+  cudaFree(dev_x);
 }
 
 TEST(RKEmbeddedCudaTest, ComputeErrorTargetTestLarge) {
   auto constexpr n_var = 1 << 20;
-  // auto const tolerance =
-  //     std::numeric_limits<double>::epsilon() * std::log2(n_var);
-  auto host_x = std::make_unique<std::array<double, n_var>>();
-  std::iota(host_x->begin(), host_x->end(), 0.0);
-  auto host_rtol = std::make_unique<std::array<double, n_var>>();
-  std::iota(host_rtol->begin(), host_rtol->end(), 1.0);
-  auto host_atol = std::make_unique<std::array<double, n_var>>();
-  std::iota(host_atol->begin(), host_atol->end(), 2.0);
-  auto dev_x = CudaState<n_var>{*host_x};
-  auto dev_rtol = CudaState<n_var>{*host_rtol};
-  auto dev_atol = CudaState<n_var>{*host_atol};
+  auto host_x = std::vector<double>(n_var);
+  std::iota(host_x.begin(), host_x.end(), 0.0);
+  auto host_rtol = std::vector<double>(n_var);
+  std::iota(host_rtol.begin(), host_rtol.end(), 1.0);
+  auto host_atol = std::vector<double>(n_var);
+  std::iota(host_atol.begin(), host_atol.end(), 2.0);
+  double* dev_x = nullptr;
+  cudaMalloc(&dev_x, n_var * sizeof(double));
+  cudaMemcpy(dev_x, host_x.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_rtol = nullptr;
+  cudaMalloc(&dev_rtol, n_var * sizeof(double));
+  cudaMemcpy(dev_rtol, host_rtol.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_atol = nullptr;
+  cudaMalloc(&dev_atol, n_var * sizeof(double));
+  cudaMemcpy(dev_atol, host_atol.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_error_target = nullptr;
+  cudaMalloc(&dev_error_target, n_var * sizeof(double));
 
-  auto dev_result = CudaState<n_var>{};
-  compute_error_target<n_var>(dev_x, dev_rtol, dev_atol, dev_result);
+  cuda_compute_error_target<<<num_blocks<n_var>(), block_size>>>(
+      dev_x, dev_rtol, dev_atol, dev_error_target, n_var);
 
-  auto host_result = std::make_unique<std::array<double, n_var>>();
-  host_compute_error_target<n_var>(*host_x, *host_rtol, *host_atol,
-                                   *host_result);
-  auto host_cuda_result = std::make_unique<std::array<double, n_var>>();
-  dev_result.to_host(*host_cuda_result);
+  auto host_error_target = std::vector<double>(n_var);
+  cudaMemcpy(host_error_target.data(), dev_error_target, n_var * sizeof(double),
+             cudaMemcpyDeviceToHost);
   for (auto i = 0; i < n_var; ++i) {
-    // EXPECT_NEAR(host_result[i], host_cuda_result[i], tolerance);
-    EXPECT_DOUBLE_EQ((*host_result)[i], (*host_cuda_result)[i]);
+    auto host_result = host_atol[i] + host_rtol[i] * std::abs(host_x[i]);
+    EXPECT_DOUBLE_EQ(host_result, host_error_target[i]);
   }
+
+  cudaFree(dev_error_target);
+  cudaFree(dev_atol);
+  cudaFree(dev_rtol);
+  cudaFree(dev_x);
 }

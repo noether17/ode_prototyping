a26a153fcaad1196192c5a3e38ad91d3681c9496
Jonathan White <jawh@umich.edu>
2024-08-22 22:14:11 -0600

    Added progress bar to console output of animation script.

--- scripts/animate_states.py
+++ scripts/animate_states.py
@@ -8,6 +8,9 @@ animation_time = 10.0 # seconds to run animation
 max_fps = 30 # maximum frames per second
 dim = 3 # number of dimensions
 
+# For updating progress
+total_frames = 0
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
@@ -23,23 +26,21 @@ def main():
 
     #interpolate
     spline = interp.make_interp_spline(times, positions, bc_type='natural')
-    n_frames = int(max_fps * animation_time)
-    interp_times = np.linspace(times[0], times[-1], num=n_frames)
+    global total_frames
+    total_frames = int(max_fps * animation_time)
+    interp_times = np.linspace(times[0], times[-1], num=total_frames)
     interp_positions = spline(interp_times)
 
     # animate
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    ani = anim.FuncAnimation(fig, plot_frame, n_frames,
+    ani = anim.FuncAnimation(fig, plot_frame, total_frames,
                              fargs=(interp_times, interp_positions, ax))
     writer = anim.PillowWriter(fps=max_fps)
     ani.save(f"animation_{N}_particles.gif", writer=writer)
+    print() # newline after progress report
 
 def plot_frame(frame_idx, times, positions, ax):
-    #row_index = np.where(states[:, 0] == t)
-    #N = int((states.shape[1] - 1) / 6)
-    #dof = N * dim
-    #current_frame = states[row_index, 1:dof+1].reshape((N, dim))
     t = times[frame_idx]
     current_frame = positions[frame_idx]
     N = int(current_frame.size / dim)
@@ -51,6 +52,11 @@ def plot_frame(frame_idx, times, positions, ax):
     ax.set_ylim(-2, 2)
     ax.set_zlim(-2, 2)
     ax.set_title(f"Number of Particles: {N}\nt = {t:.2f}")
+
+    global total_frames
+    progress_percent = ((frame_idx + 1) / total_frames) * 100.0
+    print(f"\rPlotted {progress_percent:.2f}% of frames.", end='')
+
     return ax
 
 if __name__ == "__main__":

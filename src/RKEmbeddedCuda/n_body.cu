5c2aa6ccfa648eccc18e9270a1861c215fbfd074
Jonathan White <jawh@umich.edu>
2024-09-02 14:42:08 -0600

    Extracted initialization of state and tolerance into function.

--- src/RKEmbeddedCuda/n_body.cu
+++ src/RKEmbeddedCuda/n_body.cu
@@ -8,51 +8,16 @@
 #include "RKEmbeddedCuda.cuh"
 #include "RawCudaOutput.cuh"
 
+auto constexpr N = 16;
+auto constexpr L = 1.0;
+auto constexpr n_var = N * 6;
+auto init_state_and_tol() -> std::pair<double*, double*>;
+
 int main() {
-  // Simple Two-Body Orbit
-  // auto host_x0 =
-  //     std::array{1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, -0.5,
-  //     0.0};
-  // Three-Body Figure-8
-  // auto host_x0 =
-  //    std::array{0.9700436,   -0.24308753, 0.0, -0.9700436,  0.24308753,  0.0,
-  //               0.0,         0.0,         0.0, 0.466203685, 0.43236573,  0.0,
-  //               0.466203685, 0.43236573,  0.0, -0.93240737, -0.86473146,
-  //               0.0};
-  // Pythagorean Three-Body
-  // auto host_x0 = std::array{1.0, 3.0, 0.0, -2.0, -1.0, 0.0, 1.0, -1.0, 0.0,
-  //                           0.0, 0.0, 0.0, 0.0,  0.0,  0.0, 0.0, 0.0,  0.0};
-  // Five-Body Double Figure-8
-  // auto host_x0 =
-  //    std::array{1.657666,  0.0,       0.0, 0.439775,  -0.169717, 0.0,
-  //               -1.268608, -0.267651, 0.0, -1.268608, 0.267651,  0.0,
-  //               0.439775,  0.169717,  0.0, 0.0,       -0.593786, 0.0,
-  //               1.822785,  0.128248,  0.0, 1.271564,  0.168645,  0.0,
-  //               -1.271564, 0.168645,  0.0, -1.822785, 0.128248,  0.0};
-  // auto const n_var = host_x0.size();
-  // 1024-Body Cube
-  auto constexpr N = 16;
-  auto constexpr L = 1.0;
-  auto constexpr n_var = N * 6;
-  auto host_x0 = std::vector<double>(n_var);
-  auto gen = std::mt19937{0};
-  auto dist = std::uniform_real_distribution<double>(0.0, L);
-  for (auto i = 0; i < host_x0.size() / 2; ++i) {
-    host_x0[i] = dist(gen);
-  }
   auto t0 = 0.0;
   auto tf = std::sqrt(L * L * L / N);
   std::cout << "End time = " << tf << '\n';
-  auto host_tol = std::vector<double>(n_var);
-  std::fill(host_tol.begin(), host_tol.end(), 1.0e-10);
-  double* dev_x0 = nullptr;
-  cudaMalloc(&dev_x0, n_var * sizeof(double));
-  cudaMemcpy(dev_x0, host_x0.data(), n_var * sizeof(double),
-             cudaMemcpyHostToDevice);
-  double* dev_tol = nullptr;
-  cudaMalloc(&dev_tol, n_var * sizeof(double));
-  cudaMemcpy(dev_tol, host_tol.data(), n_var * sizeof(double),
-             cudaMemcpyHostToDevice);
+  auto [dev_x0, dev_tol] = init_state_and_tol();
   auto masses = std::array<double, N>{};
   std::fill(masses.begin(), masses.end(), 1.0);
   auto ode = CudaNBodyOde<n_var>{masses, 1.0e-3};
@@ -75,4 +40,46 @@ int main() {
   cudaFree(dev_x0);
 
   return 0;
+}
+
+auto init_state_and_tol() -> std::pair<double*, double*> {
+  // Simple Two-Body Orbit
+  // auto host_x0 =
+  //     std::array{1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, -0.5,
+  //     0.0};
+  // Three-Body Figure-8
+  // auto host_x0 =
+  //    std::array{0.9700436,   -0.24308753, 0.0, -0.9700436,  0.24308753,  0.0,
+  //               0.0,         0.0,         0.0, 0.466203685, 0.43236573,  0.0,
+  //               0.466203685, 0.43236573,  0.0, -0.93240737, -0.86473146,
+  //               0.0};
+  // Pythagorean Three-Body
+  // auto host_x0 = std::array{1.0, 3.0, 0.0, -2.0, -1.0, 0.0, 1.0, -1.0, 0.0,
+  //                           0.0, 0.0, 0.0, 0.0,  0.0,  0.0, 0.0, 0.0,  0.0};
+  // Five-Body Double Figure-8
+  // auto host_x0 =
+  //    std::array{1.657666,  0.0,       0.0, 0.439775,  -0.169717, 0.0,
+  //               -1.268608, -0.267651, 0.0, -1.268608, 0.267651,  0.0,
+  //               0.439775,  0.169717,  0.0, 0.0,       -0.593786, 0.0,
+  //               1.822785,  0.128248,  0.0, 1.271564,  0.168645,  0.0,
+  //               -1.271564, 0.168645,  0.0, -1.822785, 0.128248,  0.0};
+  // auto const n_var = host_x0.size();
+  // 1024-Body Cube
+  auto host_x0 = std::vector<double>(n_var);
+  auto gen = std::mt19937{0};
+  auto dist = std::uniform_real_distribution<double>(0.0, L);
+  for (auto i = 0; i < host_x0.size() / 2; ++i) {
+    host_x0[i] = dist(gen);
+  }
+  auto host_tol = std::vector<double>(n_var);
+  std::fill(host_tol.begin(), host_tol.end(), 1.0e-10);
+  double* dev_x0 = nullptr;
+  cudaMalloc(&dev_x0, n_var * sizeof(double));
+  cudaMemcpy(dev_x0, host_x0.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  double* dev_tol = nullptr;
+  cudaMalloc(&dev_tol, n_var * sizeof(double));
+  cudaMemcpy(dev_tol, host_tol.data(), n_var * sizeof(double),
+             cudaMemcpyHostToDevice);
+  return {dev_x0, dev_tol};
 }

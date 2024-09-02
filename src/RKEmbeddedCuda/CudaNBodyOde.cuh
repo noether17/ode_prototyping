f0b9be79fbeda81dc7e199258c4815dbddd31545
Jonathan White <jawh@umich.edu>
2024-09-01 21:44:17 -0600

    Added softening parameter to CudaNBodyOde.

--- src/RKEmbeddedCuda/CudaNBodyOde.cuh
+++ src/RKEmbeddedCuda/CudaNBodyOde.cuh
@@ -7,7 +7,7 @@
 template <typename MassType>
 __global__ void cuda_n_body_acc_kernel(double const* x, double* a,
                                        MassType masses, int n_pairs,
-                                       int n_particles) {
+                                       int n_particles, double softening_2) {
   auto tid = blockIdx.x * blockDim.x + threadIdx.x;
   auto n_1_2 = n_particles - 0.5;
   while (tid < n_pairs) {
@@ -22,8 +22,9 @@ __global__ void cuda_n_body_acc_kernel(double const* x, double* a,
     auto dx = jx - ix;
     auto dy = jy - iy;
     auto dz = jz - iz;
-    auto dist = std::sqrt(dx * dx + dy * dy + dz * dz);
-    auto dist_3 = dist * dist * dist;
+    auto dist_2 = dx * dx + dy * dy + dz * dz;
+    auto dist = std::sqrt(dist_2);
+    auto dist_3 = dist * (dist * dist + softening_2);
     auto ax = dx / dist_3;
     auto ay = dy / dist_3;
     auto az = dz / dist_3;
@@ -42,16 +43,18 @@ struct CudaNBodyOde {
   auto static constexpr n_particles = n_var / 6;
   auto static constexpr n_pairs = n_particles * (n_particles - 1) / 2;
   auto static constexpr dim = 3;
-  std::array<double, n_particles> masses;
+  std::array<double, n_particles> masses{};
+  double softening_2{};
   void compute_rhs(double const* x, double* f) {
     cudaMemcpy(f, x + n_var / 2, (n_var / 2) * sizeof(double),
                cudaMemcpyDeviceToDevice);
     cudaMemset(f + n_var / 2, 0, (n_var / 2) * sizeof(double));
     cuda_n_body_acc_kernel<decltype(masses)>
-        <<<num_blocks<n_pairs>(), block_size>>>(x, f + n_var / 2, masses,
-                                                n_pairs, n_particles);
+        <<<num_blocks<n_pairs>(), block_size>>>(
+            x, f + n_var / 2, masses, n_pairs, n_particles, softening_2);
   }
 
-  CudaNBodyOde(std::array<double, n_particles> const& masses)
-      : masses{masses} {}
+  CudaNBodyOde(std::array<double, n_particles> const& ms,
+               double softening = 0.0)
+      : masses{ms}, softening_2{softening * softening} {}
 };

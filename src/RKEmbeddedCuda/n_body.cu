af0c9a8a33a83032fc5976c3103663e30f094cd3
Jonathan White <jawh@umich.edu>
2024-09-01 22:51:53 -0600

    Replaced custom definition of CudaNBodyOde in n_body.cu with library version.

--- src/RKEmbeddedCuda/n_body.cu
+++ src/RKEmbeddedCuda/n_body.cu
@@ -1,63 +1,11 @@
-#include <array>
 #include <fstream>
 #include <iostream>
 #include <random>
 #include <vector>
 
 #include "BTRKF78.hpp"
+#include "CudaNBodyOde.cuh"
 #include "RKEmbeddedCuda.cuh"
-
-template <typename MassType>
-__global__ void cuda_n_body_acc_kernel(double const* x, double* a,
-                                       MassType masses, int n_pairs,
-                                       int n_particles) {
-  auto tid = blockIdx.x * blockDim.x + threadIdx.x;
-  auto n_1_2 = n_particles - 0.5;
-  while (tid < n_pairs) {
-    auto i = static_cast<int>(n_1_2 - std::sqrt(n_1_2 * n_1_2 - 2.0 * tid));
-    auto j = tid - (n_particles - 1) * i + (i * (i + 1)) / 2 + 1;
-    auto ix = x[3 * i];
-    auto iy = x[3 * i + 1];
-    auto iz = x[3 * i + 2];
-    auto jx = x[3 * j];
-    auto jy = x[3 * j + 1];
-    auto jz = x[3 * j + 2];
-    auto dx = jx - ix;
-    auto dy = jy - iy;
-    auto dz = jz - iz;
-    auto dist = std::sqrt(dx * dx + dy * dy + dz * dz);
-    auto dist_3 = dist * (dist * dist + 1.0e-6);  // softening of 10^-3
-    auto ax = dx / dist_3;
-    auto ay = dy / dist_3;
-    auto az = dz / dist_3;
-    atomicAdd(&a[3 * i], ax * masses[j]);
-    atomicAdd(&a[3 * i + 1], ay * masses[j]);
-    atomicAdd(&a[3 * i + 2], az * masses[j]);
-    atomicAdd(&a[3 * j], -ax * masses[i]);
-    atomicAdd(&a[3 * j + 1], -ay * masses[i]);
-    atomicAdd(&a[3 * j + 2], -az * masses[i]);
-    tid += blockDim.x * gridDim.x;
-  }
-}
-
-template <int n_var>
-struct CudaNBodyOde {
-  auto static constexpr n_particles = n_var / 6;
-  auto static constexpr n_pairs = n_particles * (n_particles - 1) / 2;
-  auto static constexpr dim = 3;
-  std::array<double, n_particles> masses;
-  void compute_rhs(double const* x, double* f) {
-    cudaMemcpy(f, x + n_var / 2, (n_var / 2) * sizeof(double),
-               cudaMemcpyDeviceToDevice);
-    cudaMemset(f + n_var / 2, 0, (n_var / 2) * sizeof(double));
-    cuda_n_body_acc_kernel<decltype(masses)>
-        <<<num_blocks<n_pairs>(), block_size>>>(x, f + n_var / 2, masses,
-                                                n_pairs, n_particles);
-  }
-
-  CudaNBodyOde(std::array<double, n_particles> const& ms) : masses{ms} {}
-  CudaNBodyOde(double ms) { std::fill(masses.begin(), masses.end(), ms); }
-};
 
 template <int n_var>
 struct RawCudaOutput {
@@ -121,8 +69,9 @@ int main() {
   cudaMalloc(&dev_tol, n_var * sizeof(double));
   cudaMemcpy(dev_tol, host_tol.data(), n_var * sizeof(double),
              cudaMemcpyHostToDevice);
-  auto masses = 1.0;
-  auto ode = CudaNBodyOde<n_var>{masses};
+  auto masses = std::array<double, N>{};
+  std::fill(masses.begin(), masses.end(), 1.0);
+  auto ode = CudaNBodyOde<n_var>{masses, 1.0e-3};
   auto output = RawCudaOutput<n_var>{};
 
   cuda_integrate<n_var, BTRKF78, CudaNBodyOde<n_var>, RawCudaOutput<n_var>>(

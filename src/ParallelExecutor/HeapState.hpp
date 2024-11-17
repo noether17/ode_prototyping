d267e6b56182950a43f400c9633699b93281689d
Jonathan White <jawh@umich.edu>
2024-11-17 00:01:15 -0700

    f - added HeapState.hpp

---
+++ src/ParallelExecutor/HeapState.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <span>
+#include <utility>
+
+template <typename ValueType, int N>
+class HeapState {
+ public:
+  using StateType = std::array<ValueType, N>;
+  HeapState() : state_{std::make_unique<StateType>()} {}
+  HeapState(std::span<ValueType const, N> state)
+      : state_{std::make_unique<StateType>()} {
+    std::copy(state.begin(), state.end(), state_->begin());
+  }
+  HeapState(HeapState const& state)
+      : state_{std::make_unique<StateType>(*state.state_)} {}
+  HeapState(HeapState&& state) = default;
+  auto& operator=(HeapState const& state) {
+    if (this != &state) {
+      state_ = std::make_unique<StateType>(*state.state_);
+    }
+    return *this;
+  }
+  auto& operator=(HeapState&& state) {
+    state_ = std::move(state.state_);
+    return *this;
+  }
+  ~HeapState() = default;
+
+  auto& operator[](int i) { return (*state_)[i]; }
+  auto const& operator[](int i) const { return (*state_)[i]; }
+
+  auto* data() { return state_->data(); }
+  auto const* data() const { return state_->data(); }
+
+  operator std::span<ValueType, N>() { return *state_; }
+  operator std::span<ValueType const, N>() const { return *state_; }
+
+  auto static constexpr size() { return N; }
+
+ private:
+  std::unique_ptr<StateType> state_{};
+};

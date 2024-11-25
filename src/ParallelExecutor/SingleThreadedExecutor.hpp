1daf731a96f68c37b315bad762256ecb59a1691a
Jonathan White <jawh@umich.edu>
2024-11-25 08:19:19 -0700

    Updated SingleThreadedExecutor to use new ParallelExecutor interface.

--- src/ParallelExecutor/SingleThreadedExecutor.hpp
+++ src/ParallelExecutor/SingleThreadedExecutor.hpp
@@ -3,22 +3,19 @@
 #include <utility>
 
 struct SingleThreadedExecutor {
-  template <typename ParallelKernel, typename... Args>
-  void call_parallel_kernel(ParallelKernel kernel, int n_items,
-                            Args&&... args) {
+  template <auto parallel_kernel, typename... Args>
+  void call_parallel_kernel(int n_items, Args... args) {
     for (auto i = 0; i < n_items; ++i) {
-      kernel(i, std::forward<Args>(args)...);
+      parallel_kernel(i, std::move(args)...);
     }
   }
 
-  template <typename T, typename BinaryOp, typename TransformOp,
-            typename... Args>
-  auto transform_reduce(T init_val, BinaryOp reduce, TransformOp transform,
-                        int n_items, Args&&... transform_args) {
+  template <typename T, auto reduce, auto transform, typename... TransformArgs>
+  auto transform_reduce(T init_val, int n_items,
+                        TransformArgs... transform_args) {
     auto result = init_val;
     for (auto i = 0; i < n_items; ++i) {
-      auto transform_result =
-          transform(i, std::forward<Args>(transform_args)...);
+      auto transform_result = transform(i, std::move(transform_args)...);
       result = reduce(result, transform_result);
     }
     return result;

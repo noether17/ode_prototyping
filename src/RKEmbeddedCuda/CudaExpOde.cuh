9fce35199729bee99928b3a5ea17891062525faa
Jonathan White <jawh@umich.edu>
2024-12-27 22:48:44 -0700

    Deleted redundant RKEmbedded and RKEmbeddedCuda directories.

--- src/RKEmbeddedCuda/CudaExpOde.cuh
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-template <int n_var>
-struct CudaExpOde {
-  static void compute_rhs(double const* x, double* f) {
-    cudaMemcpy(f, x, n_var * sizeof(double), cudaMemcpyDeviceToDevice);
-  }
-};

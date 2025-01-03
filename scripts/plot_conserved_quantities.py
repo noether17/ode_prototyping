1688ebea4c8ceb2bddeeb764c4bccfe3366d484e
Jonathan White <jawh@umich.edu>
2025-01-03 15:41:31 -0700

    Parallelized computation of energies using numba and used interpolation to reduce number of points plotted.

--- scripts/plot_conserved_quantities.py
+++ scripts/plot_conserved_quantities.py
@@ -1,7 +1,9 @@
 import argparse
 import math
 import matplotlib.pyplot as plt
+import numba
 import numpy as np
+import scipy.interpolate as interp
 import struct
 
 dim = 3 # number of dimensions
@@ -12,41 +14,69 @@ def main():
     args = parser.parse_args()
     filename = args.filename
 
+    # read input file
     with open(filename, mode='rb') as data_file:
+        print("Reading input file.")
         n_times = int.from_bytes(data_file.read(8), 'little')
         n_var = int.from_bytes(data_file.read(8), 'little')
-        states = np.array([[struct.unpack('d', data_file.read(8))[0]
-                            for j in np.arange(n_var + 1)]
-                           for i in np.arange(n_times)])
-    times = states[:, 0]
-    N = int((states.shape[1] - 1) / (2 * dim))
-    positions = states[:, 1:1+N*dim]
-    velocities = states[:, 1+N*dim:-1]
+        data = np.array([[struct.unpack('d', data_file.read(8))[0]
+                          for j in np.arange(n_var + 1)]
+                         for i in np.arange(n_times)])
+    times = data[:, 0]
+    states = data[:, 1:-1]
 
-    energies = [potential_energy(pos) + kinetic_energy(vel)
-                for pos, vel in zip(positions, velocities)]
-    plt.semilogy(times[1:], np.abs((energies[1:] - energies[0]) / energies[0]))
+    energies = compute_energies(states)
+
+    # interpolate
+    max_points = 1001
+    if energies.size > max_points:
+        print(f"Number of points greater than maximum ({max_points}). Interpolating.")
+        spline = interp.make_interp_spline(times, energies, bc_type='natural')
+        interp_times = np.linspace(times[0], times[-1], num=max_points)
+        interp_energies = spline(interp_times)
+    else:
+        interp_times = times
+        interp_energies = energies
+
+    # plot
+    plt.semilogy(interp_times[1:],
+                 np.abs((interp_energies[1:] - energies[0]) / energies[0]))
     plt.xlabel(r"$t$")
     plt.ylabel(r"$\frac{E(t) - E_0}{E_0}$")
     plt.title("Fractional Change in Energy over Time")
     plt.show()
 
 # calculate potential energy for a single state. assumes all masses are 1.
+@numba.njit()
 def potential_energy(state_positions):
     N = int(state_positions.size / dim)
+    softening = 0.01 / (N * (N - 1)) ###
     energy = 0.0
     for i in np.arange(N):
         for j in np.arange(i + 1, N):
             pos_i = state_positions[i*dim:(i+1)*dim]
             pos_j = state_positions[j*dim:(j+1)*dim]
-            energy -= 1.0 / np.linalg.norm(pos_j - pos_i)
+            dr = np.linalg.norm(pos_j - pos_i)
+            energy -= np.arctan(dr / softening) / softening
     return energy
 
 # calculate kinetic energy for a single state. assumes all masses are 1.
+@numba.njit()
 def kinetic_energy(state_velocities):
     N = int(state_velocities.size / dim)
     energy = np.sum(state_velocities*state_velocities) / 2.0
     return energy
+
+@numba.njit(parallel=True)
+def compute_energies(states):
+    print("Computing energies.")
+    n_states = states.shape[0]
+    offset = int(states.shape[1] / 2)
+    energies = np.zeros(n_states)
+    for i in numba.prange(n_states):
+        energies[i] = potential_energy(states[i, :offset]) + \
+                kinetic_energy(states[i, offset:])
+    return energies
 
 if __name__ == "__main__":
     main()

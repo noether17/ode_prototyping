46310ad463ea45f7e94a377830b3ada488d6429b
Jonathan White <jawh@umich.edu>
2025-01-04 11:37:57 -0700

    Added softening to binary output.

--- src/demo/n_body_benchmark_test.cu
+++ src/demo/n_body_benchmark_test.cu
@@ -53,13 +53,16 @@ auto generate_filename(auto const& scenario) {
   return filename;
 }
 
-void output_to_file(std::string const& filename, auto const& output) {
+void output_to_file(std::string const& filename, auto const& output,
+                    double softening) {
   auto const n_times = static_cast<std::size_t>(output.times.size());
   auto const n_var = static_cast<std::size_t>(output.states[0].size());
 
   auto output_file = std::ofstream{filename, std::ios::out | std::ios::binary};
   output_file.write(reinterpret_cast<char const*>(&n_times), sizeof(n_times));
   output_file.write(reinterpret_cast<char const*>(&n_var), sizeof(n_var));
+  output_file.write(reinterpret_cast<char const*>(&softening),
+                    sizeof(softening));
   for (std::size_t i = 0; i < n_times; ++i) {
     output_file.write(reinterpret_cast<char const*>(&output.times[i]),
                       sizeof(output.times[i]));
@@ -90,7 +93,7 @@ int main() {
                        output, cuda_exe);
 
   auto filename = generate_filename<BTRKF78, CudaExecutor>(scenario);
-  output_to_file(filename, output);
+  output_to_file(filename, output, scenario.softening);
 
   return 0;
 }

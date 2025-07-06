09c5761c06858606c041a679c96241a9ba0858e9
Jonathan White <jawh@umich.edu>
2025-07-05 21:42:16 -0600

    Modified plot_n_body_states.py script to plot the states for a provided binary state file.

--- scripts/plot_n_body_states.py
+++ scripts/plot_n_body_states.py
@@ -1,46 +1,25 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+from nbody_io import bin_reader as br
+
 def main():
-    #cpu_data = np.loadtxt("RKF78_n_body_output.txt", delimiter=',')
-    #cpu_times = cpu_data[:, 0]
-    #state_offset = int((cpu_data.shape[1] - 1) / 2)
-    #cpu_pos = cpu_data[:, 1:1+state_offset]
-    #cpu_vel = cpu_data[:, 1+state_offset:]
-    #n_particles = int((cpu_data.shape[1] - 1) / 6)
-    #for i in np.arange(n_particles):
-    #    plt.plot(cpu_pos[:, 3*i], cpu_pos[:, 3*i + 1], label=f"Particle {i}")
-    #plt.title("CPU Trajectories")
-    #plt.xlabel("x")
-    #plt.ylabel("y")
-    #plt.legend()
-    #plt.gca().set_aspect('equal')
-    #plt.show()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    filename = args.filename
 
-    #cpu_parallel_data = np.loadtxt("RKF78_threadpool_n_body_output.txt", delimiter=',')
-    #cpu_parallel_times = cpu_parallel_data[:, 0]
-    #state_offset = int((cpu_parallel_data.shape[1] - 1) / 2)
-    #cpu_parallel_pos = cpu_parallel_data[:, 1:1+state_offset]
-    #cpu_parallel_vel = cpu_parallel_data[:, 1+state_offset:]
-    #n_particles = int((cpu_parallel_data.shape[1] - 1) / 6)
-    #for i in np.arange(n_particles):
-    #    plt.plot(cpu_parallel_pos[:, 3*i], cpu_parallel_pos[:, 3*i + 1], label=f"Particle {i}")
-    #plt.title("CPU Parallel Trajectories")
-    #plt.xlabel("x")
-    #plt.ylabel("y")
-    #plt.legend()
-    #plt.gca().set_aspect('equal')
-    #plt.show()
-
-    cuda_data = np.loadtxt("leapfrog_cuda_n_body_output.txt", delimiter=',')
-    cuda_times = cuda_data[:, 0]
-    state_offset = int((cuda_data.shape[1] - 1) / 2)
-    cuda_pos = cuda_data[:, 1:1+state_offset]
-    cuda_vel = cuda_data[:, 1+state_offset:]
-    n_particles = int((cuda_data.shape[1] - 1) / 6)
+    states, softening = br.read_states(filename)
+    dof = int((states.shape[1] - 1) / 2) # degrees of freedom
+    times = states[:, 0]
+    positions = states[:, 1:dof + 1]
+    velocities = states[:, dof + 1:]
+    n_particles = int((states.shape[1] - 1) / 6)
     for i in np.arange(n_particles):
-        plt.plot(cuda_pos[:, 3*i], cuda_pos[:, 3*i + 1], label=f"Particle {i}")
-    plt.title("CUDA Trajectories")
+        plt.plot(positions[:, 3 * i], positions[:, 3 * i + 1],
+                 label=f"Particle {i}")
+    plt.title(f"Trajectories for {filename}", wrap=True)
     plt.xlabel("x")
     plt.ylabel("y")
     plt.legend()
